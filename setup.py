6f0be75fc2988d632b22d10cd617e94e04b27567
Matthias Steiner <matthias.steiner@geo.tuwien.ac.at>
2022-07-11 14:42:35 +0200

    added setup.py and modified the Makefile for installing/building the library

---
+++ setup.py
@@ -0,0 +1,48 @@
+"""
+Build and install the package.
+"""
+from setuptools import find_packages, setup
+
+NAME = 'formikoj'
+FULLNAME = NAME
+AUTHOR = "Matthias Steiner"
+AUTHOR_EMAIL = 'matthias.steiner@geo.tuwien.ac.at'
+LICENSE = "Lesser General Public License (LGPL)"
+URL = ""
+DESCRIPTION = ""
+LONG_DESCRIPTION = DESCRIPTION
+
+VERSION = '0.1'
+
+PACKAGES = find_packages()
+SCRIPTS = []
+
+CLASSIFIERS = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Science/Research",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Education",
+    "Topic :: Scientific/Engineering",
+    "Topic :: Software Development :: Libraries",
+    "Programming Language :: Python :: 3.7",
+    "License :: OSI Approved :: {}".format(LICENSE),
+]
+PLATFORMS = "Any"
+INSTALL_REQUIRES = []
+
+if __name__ == '__main__':
+    setup(name=NAME, 
+          fullname=FULLNAME, 
+          description=DESCRIPTION,
+          long_description=LONG_DESCRIPTION, 
+          # ~ version=VERSION, 
+          author=AUTHOR,
+          author_email=AUTHOR_EMAIL, 
+          license=LICENSE, url=URL,
+          platforms=PLATFORMS, 
+          scripts=SCRIPTS, 
+          packages=PACKAGES,
+          classifiers=CLASSIFIERS, 
+          install_requires=INSTALL_REQUIRES,
+          setuptools_git_versioning={"enabled": True},
+          setup_requires=["setuptools-git-versioning"])
