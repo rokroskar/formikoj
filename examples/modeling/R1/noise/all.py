186e4c008ce9684b256de6ac674c49d385e3217e
Matthias Steiner <matthias.steiner@geo.tuwien.ac.at>
2023-03-03 18:34:17 +0100

    Created a new synthetic data set and modified the manuscript

--- examples/modeling/R1/noise/all.py
+++ examples/modeling/R1/noise/all.py
@@ -60,89 +60,90 @@ swm = SeismicWaveformModeler('.')
 
 swm.load('mesh')
 swm.load('config')
+# ~ swm.create('waveforms')
 swm.create('traveltimes')
 
-#### INVERT SYNTHETIC TRAVEL TIMES ####
+# ~ #### INVERT SYNTHETIC TRAVEL TIMES ####
 
-# read the pck file
-srtData = pg.DataContainer("./out/syn_data/syn_data_tt.pck", 
-                           sensorTokens="s g",
-                           removeInvalid=True)
-abserr = 0.0035 # absolute error of traveltimes (s)
-srtData.set('err', np.ones(srtData.size()) * abserr)
-srtData.set('t', srtData['t']-.02)
-srtData.markInvalid(srtData['t'] <= 0.)
-srtData.removeInvalid()
+# ~ # read the pck file
+# ~ srtData = pg.DataContainer("./out/syn_data/syn_data_tt.pck", 
+                           # ~ sensorTokens="s g",
+                           # ~ removeInvalid=True)
+# ~ abserr = 0.0035 # absolute error of traveltimes (s)
+# ~ srtData.set('err', np.ones(srtData.size()) * abserr)
+# ~ srtData.set('t', srtData['t']-.02)
+# ~ srtData.markInvalid(srtData['t'] <= 0.)
+# ~ srtData.removeInvalid()
 
-# create TravelTimeManger object
-mgr = TravelTimeManager(srtData)
+# ~ # create TravelTimeManger object
+# ~ mgr = TravelTimeManager(srtData)
 
-# create mesh
-plc = mt.createParaMeshPLC(srtData.sensorPositions(),
-                           paraDX=0.25, # number of nodes between stations (sensor positions)
-                           paraDepth=15, # model depth (m) (0 --> estimated)
-                           paraBoundary=2, # margin for parameter domain 
-                                           # (sensor spacing)
-                           paraMaxCellSize=.5, # maximum cell area (m^2)
-                           boundary=0) # boundary around the parameter domain
-                                       # no need to worry about this here
-mesh = mt.createMesh(plc,
-                     quality=32, # 2D triangle quality
-                                 # recommended values: 30 - 34
-                     smooth=(1, 10)) # smoothing alogrithm - no need to change this
+# ~ # create mesh
+# ~ plc = mt.createParaMeshPLC(srtData.sensorPositions(),
+                           # ~ paraDX=0.25, # number of nodes between stations (sensor positions)
+                           # ~ paraDepth=15, # model depth (m) (0 --> estimated)
+                           # ~ paraBoundary=2, # margin for parameter domain 
+                                           # ~ # (sensor spacing)
+                           # ~ paraMaxCellSize=.5, # maximum cell area (m^2)
+                           # ~ boundary=0) # boundary around the parameter domain
+                                       # ~ # no need to worry about this here
+# ~ mesh = mt.createMesh(plc,
+                     # ~ quality=32, # 2D triangle quality
+                                 # ~ # recommended values: 30 - 34
+                     # ~ smooth=(1, 10)) # smoothing alogrithm - no need to change this
 
-# set the mesh
-mgr.setMesh(mesh,
-            secNodes=3) # secondary nodes to refine the mesh to
-                        # permit a more accurate ray tracing
+# ~ # set the mesh
+# ~ mgr.setMesh(mesh,
+            # ~ secNodes=3) # secondary nodes to refine the mesh to
+                        # ~ # permit a more accurate ray tracing
 
-# define the inversion parameters
-lam = 10 # use different values
-zWeight = 1 # isotropic smoothing
+# ~ # define the inversion parameters
+# ~ lam = 10 # use different values
+# ~ zWeight = 1 # isotropic smoothing
                         
-# run inversion
-vest = mgr.invert(vTop=500, # velocity at the top of the initial model (m/s)
-                  vBottom=5000, # velocity at the bottom of the initial model (m/s)
-                  lam=lam, # regularization parameter
-                  zWeight=zWeight, # 1 - same smoothing in x and z direction
-                                   # 0.25 - four times stronger smoothing in 
-                                   #        in horizontal direction
-                  robustData=False, verbose=True) # use iterative re-weighting
+# ~ # run inversion
+# ~ vest = mgr.invert(vTop=500, # velocity at the top of the initial model (m/s)
+                  # ~ vBottom=5000, # velocity at the bottom of the initial model (m/s)
+                  # ~ lam=lam, # regularization parameter
+                  # ~ zWeight=zWeight, # 1 - same smoothing in x and z direction
+                                   # ~ # 0.25 - four times stronger smoothing in 
+                                   # ~ #        in horizontal direction
+                  # ~ robustData=False, verbose=True) # use iterative re-weighting
 
-# Consider the ray coverage in the plot, i.e.,
-# blank areas in the model that should not be
-# interpreted.
-scov = mgr.standardizedCoverage()
-vest = np.ma.masked_where(scov == 0, vest)
+# ~ # Consider the ray coverage in the plot, i.e.,
+# ~ # blank areas in the model that should not be
+# ~ # interpreted.
+# ~ scov = mgr.standardizedCoverage()
+# ~ vest = np.ma.masked_where(scov == 0, vest)
 
-# plot the inversion result
-cMin = 750
-cMax = 4000
+# ~ # plot the inversion result
+# ~ cMin = 750
+# ~ cMax = 4000
 
-fig, ax = pg.plt.subplots(1, figsize=(8, 5))
+# ~ fig, ax = pg.plt.subplots(1, figsize=(8, 5))
 
-_, cb = pg.show(mesh, # mesh
-                vest, # resolved velocities
-                coverage=scov, # blank areas with low coverage
-                # ~ cMap='inferno_r', # colormap
-                cMap='Spectral', # colormap
-                # ~ cMap='plasma', # colormap
-                logScale=False, # illustrate log-scaled velocities
-                cMin=cMin, # lower limit of colormap
-                cMax=cMax, # upper limit of colormap
-                colorBar=True, # show colorbar
-                orientation='horizontal', # orientation of the colorbar
-                ax=ax, # axis object to plot in
-                fitView=True, # automatic aspect ratio
-                showBoundary=False) # plot mesh boundaries
+# ~ _, cb = pg.show(mesh, # mesh
+                # ~ vest, # resolved velocities
+                # ~ coverage=scov, # blank areas with low coverage
+                # cMap='inferno_r', # colormap
+                # ~ cMap='Spectral', # colormap
+                # cMap='plasma', # colormap
+                # ~ logScale=False, # illustrate log-scaled velocities
+                # ~ cMin=cMin, # lower limit of colormap
+                # ~ cMax=cMax, # upper limit of colormap
+                # ~ colorBar=True, # show colorbar
+                # ~ orientation='horizontal', # orientation of the colorbar
+                # ~ ax=ax, # axis object to plot in
+                # ~ fitView=True, # automatic aspect ratio
+                # ~ showBoundary=False) # plot mesh boundaries
 
-pg.show(geom, ax=ax, fillRegion=False, regionMarker=False)
+# ~ pg.show(geom, ax=ax, fillRegion=False, regionMarker=False)
 
-# format plot
-ax.set_xlabel('x (m)')
-ax.set_ylabel('z (m)')
-ax.spines['top'].set_visible(False)
-ax.spines['right'].set_visible(False)
-cb.set_label('v$_p$ ($\mathregular{m\/s^{-1}}$)', fontsize=10)
+# ~ # format plot
+# ~ ax.set_xlabel('x (m)')
+# ~ ax.set_ylabel('z (m)')
+# ~ ax.spines['top'].set_visible(False)
+# ~ ax.spines['right'].set_visible(False)
+# ~ cb.set_label('v$_p$ ($\mathregular{m\/s^{-1}}$)', fontsize=10)
 
-pg.plt.show()
+# ~ pg.plt.show()
